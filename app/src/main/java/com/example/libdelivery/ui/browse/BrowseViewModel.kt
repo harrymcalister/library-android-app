41a271969d55047e54169bee67e97d1490b54d3a
Harry McAlister <harry.mcalist@gmail.com>
2022-12-26 17:40:18 +0000

    Modify the Nav Component and Fragments to suit the app

--- app/src/main/java/com/example/libdelivery/ui/browse/BrowseViewModel.kt
+++ app/src/main/java/com/example/libdelivery/ui/browse/BrowseViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.libdelivery.ui.notifications
+package com.example.libdelivery.ui.browse
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class BrowseViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is browse Fragment"
     }
     val text: LiveData<String> = _text
 }
--- app/src/main/java/com/example/libdelivery/ui/orders/OrdersViewModel.kt
+++ app/src/main/java/com/example/libdelivery/ui/orders/OrdersViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.libdelivery.ui.dashboard
+package com.example.libdelivery.ui.orders
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class OrdersViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is orders Fragment"
     }
     val text: LiveData<String> = _text
 }
